d61c76613b5d527869d43260a448fd0ee867c173
kyruodioso <kanu_nirvana@hotmail.com>
2024-04-30 08:46:26 -0300

    base del proyecto

---
+++ camera.js
@@ -0,0 +1,20 @@
+'use strict'
+
+window.onload=()=>{
+    const addImage=document.getElementById("add-image");
+    const addPost=document.getElementById("addPost");
+
+
+    addPost.addEventListener("click",()=>{
+        postPost()
+    }
+    )
+}
+
+const captureImage=()=>{
+    (alert("agregando imagen"))
+}
+
+const postPost=()=>{
+    (alert("nuevo post agregado"))
+}
