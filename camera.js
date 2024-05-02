7ce09083e43719b2eb57662fa19bc6d6dc8b4ad8
kyruodioso <kanu_nirvana@hotmail.com>
2024-05-02 17:11:11 -0300

    add camera

--- camera.js
+++ camera.js
@@ -1,20 +1,37 @@
 'use strict'
 
 window.onload=()=>{
-    const addImage=document.getElementById("add-image");
-    const addPost=document.getElementById("addPost");
-
-
-    addPost.addEventListener("click",()=>{
-        postPost()
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const captureImage = document.getElementById("captureImage");
+    const errorMsgElement = document.querySelector('span#errorMsg');
+    
+    const constraints = {
+      audio: false,
+      video: {
+        width: 250, height: 220
+      }
+    };
+    
+    //init webcam
+    async function init() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+      } catch (e) {
+        errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+      }
     }
-    )
-}
-
-const captureImage=()=>{
-    (alert("agregando imagen"))
-}
-
-const postPost=()=>{
-    (alert("nuevo post agregado"))
+    
+    function handleSuccess(stream) {
+      window.stream = stream;
+      video.srcObject = stream;
+    }
+    
+    init();
+    
+    var context = canvas.getContext('2d');
+    captureImage.addEventListener("click", function() {
+            context.drawImage(video, 0, 0, 250, 220);
+    });
 }
